79bf87bbe7435192e4a57b42340b89c54d024e29
ayberkgerey <ayberkgerey@gmail.com>
2020-10-05 21:53:59 +0300

    Jupiter Dependencies has been added.

--- src/test/java/com/geray/farmerserver/FarmerserverApplicationTests.java
+++ src/test/java/com/geray/farmerserver/FarmerserverApplicationTests.java
@@ -1,13 +1,19 @@
 package com.geray.farmerserver;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class FarmerserverApplicationTests {
 
     @Test
-    void contextLoads() {
+    void getTest(){
+        assertEquals(1,1);
     }
 
 }
