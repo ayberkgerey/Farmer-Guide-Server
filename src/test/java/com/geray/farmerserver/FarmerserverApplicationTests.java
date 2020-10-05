f4682feea39eb5778eab6a87046896ec9be74592
ayberkgerey <ayberkgerey@gmail.com>
2020-10-05 22:18:13 +0300

    .travis.yml file's classpath has been changed

--- src/test/java/com/geray/farmerserver/FarmerserverApplicationTests.java
+++ src/test/java/com/geray/farmerserver/FarmerserverApplicationTests.java
@@ -2,18 +2,26 @@ package com.geray.farmerserver;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+
+@WebMvcTest(FarmerserverApplication.class)
 @RunWith(SpringRunner.class)
 class FarmerserverApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void getTest(){
-        assertEquals(1,1);
+    void getTest() throws Exception {
+        mockMvc.perform(get("/1")).andExpect(status().isOk());
     }
 
 }
